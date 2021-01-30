48bd746226166488a8e7ffce5ed2b083c27f5e95
KobsCode <65038840+KobsCode@users.noreply.github.com>
2021-01-30 23:47:16 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ module.exports = client => {
   //online = çevrimiçi
   console.log(`${client.user.id}                                                                                                                                                                     `)
   //client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
-client.user.setActivity(`Lastarmy V12 Kayıt Sistemi`, { type: "WATCHING"});  
+client.user.setActivity(`Rivals V12 Kayıt Sistemi`, { type: "WATCHING"});  
 //LISTENING = DİNLİYOR
   //WATCHING = İZLİYOR
   //PLAYING = OYNUYOR 
